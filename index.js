22b3683d790e0bcea21dc74b35707ad612064a48
JJSamuel343 <joshuajs92@gmail.com>
2020-07-24 01:23:52 +0800

    removed dependencies

--- index.js
+++ index.js
@@ -1,19 +1,47 @@
-var request = require('request');
+
+document.getElementById('searchBtn').addEventListener("click",function() {
+    const searchQuery = document.getElementById('searchQuery').innerHTML;
+    const url = 'https://www.parsehub.com/api/v2/projects/tKOTHZu4srR8/run';
+    // Data to be sent to the API
+    let data = {
+        search : searchQuery
+    }
+
+    let fetchData = {
+        method: 'POST',
+        form: {
+            api_key: "tAtNUA5xWWyC",
+            start_url: "https://www.propertyguru.com.my/",
+            start_template: "main_template",
+            start_value_override: data,
+            send_email: "1"
+        }
+    }
+    
+    fetch(url,fetchData)
+        .then(function(err, resp, body) {
+            console.log(body);
+        });
+
+})
+
+/*
+
 
 request({
     uri: 'https://www.parsehub.com/api/v2/projects/tKOTHZu4srR8/run',
     method: 'POST',
     form: {
       api_key: "tAtNUA5xWWyC",
-      start_url: "http://www.example.com",
+      start_url: "https://www.propertyguru.com.my/",
       start_template: "main_template",
-      start_value_override: "{\"query\": \"San Francisco\"}",
+      start_value_override: "{\"search\": \" \"}",
       send_email: "1"
     }
   }, function(err, resp, body) {
     console.log(body);
   });
-
+*/
 /*
 // Set up express app
 const app = express();
